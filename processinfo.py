e5ab48a2632b020c49137325df44f7329e33942e
Stephegbenga <78284135+Stephegbenga@users.noreply.github.com>
2022-04-24 03:02:22 +0000

    update

--- processinfo.py
+++ processinfo.py
@@ -11,23 +11,29 @@ def getdatafromsheet():
     client = gspread.authorize(creds)
     sheet = client.open("Copie de Recherche nounou").sheet1
     results = sheet.get_all_records()
+    # print(results)
     for result in results:
         data_array = f"{result['type_de_poste']}, {result['ville']}, {result['jours de semaines']}, {result['optionsicouchante']}, {result['optionsireguliere']}".replace(',', '').split()
-        data = {"name":result['nom'],"data":data_array}
+        data = {"name":result['nom'],"ref_no":result['Référence'],"data":data_array}
         response.append(data)
     return response
 
 def getsimilarwords(incomingwords):
     result = []
+    final_result =[]
     words = getdatafromsheet()
     for word in words:
         for incomingword in incomingwords:
             testt = difflib.get_close_matches(incomingword, word['data'])
             if testt != []:
                 print(f"{word['name']} --> {testt}")
-                result.append(incomingword)
+                result.append(word)
     print(result)
-    return result
+    for i in range(len(result)):
+        if result[i] not in final_result:
+            final_result.append(result[i])
+    print(final_result)
+    return final_result
 
 # information = getsimilarwords(['iiididid'])
 # print(len(information))
